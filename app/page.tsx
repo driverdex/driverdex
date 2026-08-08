c998c68176d06e562c54b3ec09ea778aa7d75dff
shouravx <rhshourav02@gmail.com>
2026-08-09 00:21:03 +0600

    added automation files

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DriverSearchDemo } from '@/components/driver-search-demo'
-
-export default function Page() {
-  return (
-    <main className="min-h-screen px-4 py-10 sm:py-16">
-      <DriverSearchDemo />
-    </main>
-  )
-}
